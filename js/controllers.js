078740f6b15b0d455d100ddcfe5596cc59891e3c
mahesh maurya <mahesh@wohlig.com>
2016-03-28 17:35:04 +0530

    tab error done

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider','ui-rangeSlider'])
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'ui-rangeSlider'])
 
 .controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
   //Used to name the .html file
@@ -40,110 +40,106 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
 })
 
 .controller('UserBookingCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("userbooking");
-  $scope.menutitle = NavigationService.makeactive("User Booking");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-.controller('EditUserCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("edit-user");
-  $scope.menutitle = NavigationService.makeactive("Edit-User");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-.controller('ContactCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("contact");
-  $scope.menutitle = NavigationService.makeactive("Contact");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-.controller('PrivacyCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("privacy");
-  $scope.menutitle = NavigationService.makeactive("Privacy");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-.controller('TermsConditionCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("terms-condition");
-  $scope.menutitle = NavigationService.makeactive("Terms-Condition");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-.controller('HomeExpertCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("home-expert");
-  $scope.menutitle = NavigationService.makeactive("Home-Expert");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-.controller('ExpertProfileCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("expert-profile");
-  $scope.menutitle = NavigationService.makeactive("Expert-Profile");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
+    $scope.template = TemplateService.changecontent("userbooking");
+    $scope.menutitle = NavigationService.makeactive("User Booking");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+  })
+  .controller('EditUserCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("edit-user");
+    $scope.menutitle = NavigationService.makeactive("Edit-User");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+  })
+  .controller('ContactCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("contact");
+    $scope.menutitle = NavigationService.makeactive("Contact");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+  })
+  .controller('PrivacyCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("privacy");
+    $scope.menutitle = NavigationService.makeactive("Privacy");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+  })
+  .controller('TermsConditionCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("terms-condition");
+    $scope.menutitle = NavigationService.makeactive("Terms-Condition");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+  })
+  .controller('HomeExpertCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("home-expert");
+    $scope.menutitle = NavigationService.makeactive("Home-Expert");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+  })
+  .controller('ExpertProfileCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("expert-profile");
+    $scope.menutitle = NavigationService.makeactive("Expert-Profile");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
 
-  $scope.tab = 'personal';
-$scope.classa = 'active';
-$scope.classb = '';
-$scope.classc = '';
-$scope.classd = '';
+    $scope.tab2 = 'personal';
+    $scope.classa = 'active';
+    $scope.classb = '';
+    $scope.classc = '';
+    $scope.classd = '';
 
-$scope.tabchange = function(tab, a) {
-    //        console.log(tab);
-    $scope.tab = tab;
-    if (a == 1) {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = "active";
+    $scope.tabchange = function(tab, a) {
+             console.log(tab);
+      $scope.tab2 = tab;
+      if (a == 1) {
+   $scope.classa = "active";
         $scope.classb = '';
         $scope.classc = '';
         $scope.classd = '';
-    } else if (a == 2) {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = '';
+      } else if (a == 2) {
+         $scope.classa = '';
         $scope.classb = "active";
         $scope.classc = '';
         $scope.classd = '';
-    }else if (a == 3) {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = '';
+      } else if (a == 3) {
+         $scope.classa = '';
         $scope.classb = '';
         $scope.classc = "active";
         $scope.classd = '';
-    }else {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = '';
+      } else {
+         $scope.classa = '';
         $scope.classb = '';
         $scope.classc = '';
         $scope.classd = "active";
-    }
-};
-})
-.controller('FaqCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("faq");
-  $scope.menutitle = NavigationService.makeactive("FAQ");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-.controller('SearchCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  $scope.template = TemplateService.changecontent("search");
-  $scope.menutitle = NavigationService.makeactive("Search");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-  $scope.demo2 = {
+      }
+    };
+  })
+  .controller('FaqCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("faq");
+    $scope.menutitle = NavigationService.makeactive("FAQ");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+  })
+  .controller('SearchCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("search");
+    $scope.menutitle = NavigationService.makeactive("Search");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+    $scope.demo2 = {
       range: {
-          min: 0,
-          max: 10050
+        min: 0,
+        max: 10050
       },
       minPrice: 1000,
       maxPrice: 4000
-  };
-  // set available range
-$scope.minPrice = 100;
-$scope.maxPrice = 999;
+    };
+    // set available range
+    $scope.minPrice = 100;
+    $scope.maxPrice = 999;
 
-// default the user's values to the available range
-$scope.userMinPrice = $scope.minPrice;
-$scope.userMaxPrice = $scope.maxPrice;
-})
+    // default the user's values to the available range
+    $scope.userMinPrice = $scope.minPrice;
+    $scope.userMaxPrice = $scope.maxPrice;
+  })
 
 .controller('ProfileCtrl', function($scope, TemplateService, NavigationService, $timeout) {
   $scope.template = TemplateService.changecontent("profile");
@@ -151,41 +147,37 @@ $scope.userMaxPrice = $scope.maxPrice;
   TemplateService.title = $scope.menutitle;
   $scope.navigation = NavigationService.getnav();
 
-  $scope.tab = 'summary';
-$scope.classa = 'active';
-$scope.classb = '';
-$scope.classc = '';
-$scope.classd = '';
+  $scope.tab2 = 'summary';
+  $scope.classa = 'active';
+  $scope.classb = '';
+  $scope.classc = '';
+  $scope.classd = '';
 
-$scope.tabchange = function(tab, a) {
-    //        console.log(tab);
-    $scope.tab = tab;
+  $scope.tabchange = function(tab, a) {
+           console.log(tab);
+    $scope.tab2 = tab;
     if (a == 1) {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = "active";
-        $scope.classb = '';
-        $scope.classc = '';
-        $scope.classd = '';
+    $scope.classa = "active";
+      $scope.classb = '';
+      $scope.classc = '';
+      $scope.classd = '';
     } else if (a == 2) {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = '';
-        $scope.classb = "active";
-        $scope.classc = '';
-        $scope.classd = '';
-    }else if (a == 3) {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = '';
-        $scope.classb = '';
-        $scope.classc = "active";
-        $scope.classd = '';
-    }else {
-        $ionicScrollDelegate.scrollTop();
-        $scope.classa = '';
-        $scope.classb = '';
-        $scope.classc = '';
-        $scope.classd = "active";
+     $scope.classa = '';
+      $scope.classb = "active";
+      $scope.classc = '';
+      $scope.classd = '';
+    } else if (a == 3) {
+   $scope.classa = '';
+      $scope.classb = '';
+      $scope.classc = "active";
+      $scope.classd = '';
+    } else {
+   $scope.classa = '';
+      $scope.classb = '';
+      $scope.classc = '';
+      $scope.classd = "active";
     }
-};
+  };
 })
 
 .controller('headerctrl', function($scope, TemplateService) {
