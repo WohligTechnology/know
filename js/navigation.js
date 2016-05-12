08a2e5eea4cbf4ae514621a9238df27ab0bd0361
dhavalgala <dhaval.gala59@gmail.com>
2016-05-12 15:32:01 +0530

    footer,menu links done

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,6 @@
 var adminurl = "http://192.168.1.115:1337/";
 // var adminurl1 = "http://wohlig.io:81/callApi/jacknows/";
-var imgurl = "http://192.168.1.112:1337/upload/";
+var imgurl = "http://192.168.1.112:81/upload/";
 var imgpath = imgurl + "readFile";
 var uploadurl = imgurl;
 
@@ -16,12 +16,14 @@ var navigationservice = angular.module('navigationservice', [])
         //   classis: "active",
         //   link: "#/home"
         // }]
-    }, {
-        name: "My Calendar",
-        classis: "active",
-        anchor: "home",
-
-    }, {
+    },
+    //  {
+    //     name: "My Calendar",
+    //     classis: "active",
+    //     anchor: "home",
+    //
+    // },
+     {
         name: "My Booking Page",
         classis: "active",
         anchor: "home",
